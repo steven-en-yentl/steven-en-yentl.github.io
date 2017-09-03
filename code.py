2bb22f601bcd9b9d5dea02b2d2a4fbc3c6bbbbd7
Joris Gillis; u0052373 <joris.gillis@kuleuven.be>
2017-09-03 15:49:36 +0200

    set up

--- code.py
+++ code.py
@@ -1,9 +1,25 @@
 import sys
-code = sys.argv[1]
-keywords = sys.argv[2:]
+import hashlib
+
+hasher = hashlib.md5
+
+box_code = sys.argv[1]
+keywords = sys.argv[2:-1]
+youtube  = sys.argv[-1]
+
 seed = "Zo makkelijk gaan we het niet maken he Steven."
 
-digest = "".join(keywords)
+to_digest = "".join(keywords)
 
-print "check", hash(digest)
-print "" hash(seed+digest)
+goal_byte = map(ord,box_code+youtube)
+
+print "var check_hash = %s;" % map(ord,hasher(to_digest).digest())
+r_byte = map(ord,hasher(seed+to_digest).digest())
+offset_byte = [ (j-i) % 256 for i,j in zip(r_byte,goal_byte)]
+print "var offset = %s;" % str(list(offset_byte))
+
+print  [ chr((i+j) % 256) for i,j in zip(r_byte,offset_byte)]
+
+
+
+
